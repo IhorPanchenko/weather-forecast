b3609abcd1e290b0db4e2b0d769ec55426aca24f
Ihor Panchenko <ipanchenko91@gmail.com>
2024-10-07 13:11:59 +0200

    Create a history of last 5 searches

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,12 +4,31 @@ import { fetchWeather } from "../features/weather/weatherSlice";
 
 const Weather = () => {
   const [city, setCity] = useState("");
+  const [history, setHistory] = useState([]);
   const dispatch = useDispatch();
   const { weatherData, status, error } = useSelector((state) => state.weather);
 
   const handleFetchWeather = () => {
     if (city) {
-      dispatch(fetchWeather(city));
+      dispatch(fetchWeather(city)).then((response) => {
+        if (response.payload) {
+          setHistory((prevHistory) => {
+            const newHistory = [
+              ...prevHistory,
+              {
+                name: response.payload.name,
+                temp: response.payload.main.temp,
+                weather: response.payload.weather[0].description,
+              },
+            ];
+            console.log("11111" + newHistory.name);
+            if (newHistory.length > 5) {
+              newHistory.shift();
+            }
+            return newHistory;
+          });
+        }
+      });
     }
   };
   console.log(weatherData);
@@ -39,9 +58,26 @@ const Weather = () => {
           <h2>{weatherData.name}</h2>
           <p>Temperature: {weatherData.main.temp}°C</p>
           <p>Weather: {weatherData.weather[0].description}</p>
+          <img
+            src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+            alt="weather icon"
+          />
         </div>
       )}
       {status === "failed" && <p className="error-message">Error: {error}</p>}
+
+      {history.length > 0 && (
+        <div className="search-history">
+          <h3>Last 5 searches:</h3>
+          <ul>
+            {history.map((item, index) => (
+              <li key={index}>
+                <strong>{item.name}:</strong> {item.temp}°C, {item.weather}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
