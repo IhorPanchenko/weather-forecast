803fc9b642827595f051dc7a4306717b10673c20
Ihor Panchenko <ipanchenko91@gmail.com>
2024-10-07 16:57:56 +0200

    Add initial state for weatherSlice

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,29 @@
+const TEMPERATURE_THRESHOLDS = {
+  VERY_COLD: 0,
+  COLD: 5,
+  COOL: 15,
+  MILD: 20,
+  WARM: 25,
+};
+
+/**
+ * Get the weather class based on temperature.
+ *
+ * @param {number} temp - The temperature in degrees Celsius.
+ * @returns {string} - The corresponding weather class: "veryCold", "cold", "cool", "mild", "warm", "hot", or "unknown".
+ */
 export const getWeatherClass = (temp) => {
-  if (temp > 15) return "warm";
-  if (temp > 5 && temp < 15) return "fall";
-  return "cold";
+  if (temp < TEMPERATURE_THRESHOLDS.VERY_COLD) return "veryCold"; // Below 0
+  if (
+    temp >= TEMPERATURE_THRESHOLDS.VERY_COLD &&
+    temp <= TEMPERATURE_THRESHOLDS.COLD
+  )
+    return "cold"; // 0 to 5
+  if (temp > TEMPERATURE_THRESHOLDS.COLD && temp <= TEMPERATURE_THRESHOLDS.COOL)
+    return "cool"; // 6 to 15
+  if (temp > TEMPERATURE_THRESHOLDS.COOL && temp <= TEMPERATURE_THRESHOLDS.MILD)
+    return "mild"; // 16 to 20
+  if (temp > TEMPERATURE_THRESHOLDS.MILD && temp <= TEMPERATURE_THRESHOLDS.WARM)
+    return "warm"; // 21 to 25
+  return "hot";
 };
